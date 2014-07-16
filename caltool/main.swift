d48080c0c85e0cca2d21675c943fafcc4e779efb
Adrian Sutton <adrian@symphonious.net>
2014-07-16 12:55:58 +1000

    Initial Commit

---
+++ caltool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  caltool
+//
+//  Created by Adrian Sutton on 16/07/2014.
+//  Copyright (c) 2014 Adrian Sutton. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
